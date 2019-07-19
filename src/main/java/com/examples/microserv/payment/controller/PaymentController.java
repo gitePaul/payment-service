e47eae81c0fd6ffe97798280c546d70f78c76960
Admin <Admin@DESKTOP-6K5ADTD>
2019-07-19 23:25:28 +0530

    Clean Payment List, Tests and API Document

--- src/main/java/com/examples/microserv/payment/controller/PaymentController.java
+++ src/main/java/com/examples/microserv/payment/controller/PaymentController.java
@@ -3,22 +3,20 @@ package com.examples.microserv.payment.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
-import com.examples.microserv.payment.model.PaymentTnx;
 import com.examples.microserv.payment.model.RawPayment;
 import com.examples.microserv.payment.service.PaymentTnxService;
 import com.examples.microserv.payment.service.RawPaymentService; 
- 
+@EnableEurekaClient 
 @RestController 
-@RequestMapping("/payment")
+@RequestMapping("/payments")
 public class PaymentController {
 	
 	@Autowired 
@@ -31,27 +29,10 @@ public class PaymentController {
 	{		 
 	  return rawPaymnentService.list();
 	}
-    @PutMapping
-    public ResponseEntity<String> payBill(@RequestBody RawPayment rawPayment){
+    @PostMapping
+    public ResponseEntity<String> submitPayment(@RequestBody RawPayment rawPayment){
     	rawPaymnentService.create(rawPayment); 
     	 
 		return new ResponseEntity<String>("Paid Succesfully", HttpStatus.CREATED);
-	}
-	 
-/*	@PostMapping("/check")
-	public List<PaymentTnx> findPaymentByPspCodeAndPaymentReceipt(@RequestParam String pspCode, String paymentReceipt)
-	{
-		if(pspCode != null && paymentReceipt != null)
-			return paymentService.findPaymentByPspCodeAndPaymentReceipt(pspCode, paymentReceipt);
-		else
-			return null;
-	}	*/
-
-	/*@PostMapping("/pay-bill")
-	public ResponseEntity<String> payBill(RawPayment rawPayment){
-	    rawPaymentService.receivePayment(rawPayment);
-		return new ResponseEntity<String>("Paid Successfully", HttpStatus.OK);
-	}	
-	*/
-	
+	} 
 }
--- src/test/java/com/examples/microserv/payment/controller/PaymentControllerTest.java
+++ src/test/java/com/examples/microserv/payment/controller/PaymentControllerTest.java
@@ -1,63 +1,80 @@
 package com.examples.microserv.payment.controller;
 
 import static org.hamcrest.CoreMatchers.containsString;
-
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
+import com.examples.microserv.payment.repository.PaymentTnxRepository;
+import com.examples.microserv.payment.repository.RawPaymentRepository;
+import com.examples.microserv.payment.service.PaymentTnxService;
+import com.examples.microserv.payment.service.RawPaymentService;
+@RunWith(SpringRunner.class)
 @WebMvcTest(PaymentController.class)
-@AutoConfigureRestDocs(outputDir = "target/snippets")
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "target/snippets") 
 public class PaymentControllerTest {
-
+    
 	@Autowired
 	private MockMvc mockMvc;
-	@Autowired
-	TestRestTemplate restTemp;
+	@MockBean
+	PaymentTnxService paymentTnxService;
+	@MockBean
+	RawPaymentService rawPaymentService;	
+	@MockBean
+	PaymentTnxRepository paymentTnxRepository;
+	@MockBean
+    RawPaymentRepository rawPaymentRepository;	
+	Logger log = LoggerFactory.getLogger(PaymentControllerTest.class);
 	@Test
-	public void shouldGetAllPayments() throws Exception {
-		Logger log = LoggerFactory.getLogger(PaymentControllerTest.class);
+	public void shouldGetAllPayments() throws Exception {		
  
-		log.info("\n--------------------All Payments---------------------------\n{}", mockMvc.toString());
+		log.info("\n--------------------LIST ALL PAYMENTS---------------------------\n{}",mockMvc);
 
-		mockMvc.perform(get("/payment").contentType(MediaType.APPLICATION_JSON))
+		mockMvc.perform(get("/payments").contentType(MediaType.APPLICATION_JSON))
 		.andDo(print())
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$").isNotEmpty())
-		.andDo(document("payment"));
+		.andExpect(status().isOk())	
+		.andDo(document("payments"));
+		log.info("\n--------------------END LIST PAYMENTS---------------------------\n");
 
 	}
 	@Test
-	public void shouldGetPayment() throws Exception {
+	public void shouldSubmitPayment() throws Exception {
 		Logger log = LoggerFactory.getLogger(PaymentControllerTest.class);
 
-		String body = "{\r\n" + " \"spCode\":\"SP102\",\r\n" + " \"pspCode\":\"PSP006\", \r\n"
-				+ " \"billControlNumber\":\"991021234567\",\r\n" + " \"paidAmount\":\"50000\",  \r\n"
-				+ " \"billCurrency\":\"TZS\"\r\n" + "}";
-		log.info("\n--------------------Payment Request---------------------------\n{}", body);
+		String body = "{\r\n" + 
+				"	\"spCode\":\"SP102\",\r\n" + 
+				"	\"pspCode\":\"PSP006\",  \r\n" + 
+				"	\"billControlNumber\":\"991021234567\",\r\n" + 
+				"	\"paidAmount\":\"50000\",  \r\n" + 
+				"	\"billCurrency\":\"TZS\",\r\n" + 
+				"	\"payerEmail\":\"omugamba@gmail.com\",\r\n" + 
+				"	\"paymentReceipt\":\"991021234567\",\r\n" + 
+				"	\"paidDate\":\"31-07-2019 00:00.00\",\r\n" + 
+				"	\"paymentReceivedDate\":\"31-07-2019 00:00.00\"\r\n" + 
+				"}";
+		log.info("\n--------------------RECEIVING PAYMENT---------------------------\n{}",mockMvc);
 
-		mockMvc.perform(put("/payment").contentType(MediaType.APPLICATION_JSON).content(body))
+		mockMvc.perform(post("/payments").contentType(MediaType.APPLICATION_JSON).content(body))
 		        .andDo(print())
 				.andExpect(status().isCreated())
 				.andExpect(content().string(containsString("Paid Succesfully")))
-				.andDo(document("payment"));
+				.andDo(document("make-payment"));
+		log.info("\n--------------------END RECEIVING PAYMENT---------------------------{}\n",mockMvc.perform(post("/payment").contentType(MediaType.APPLICATION_JSON).content(body)));
 
-	}
-
+	}    
 }
